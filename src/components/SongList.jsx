b95d38a0ab46ea783c1b26b8406c5575e7fa4d5c
Cherie <cherietsukayama@gmail.com>
2020-08-14 03:37:56 +0000

    Imporrted the useState hook/function/method from react and used it to set the state. Then destructured the array that useState returns with [songs, setSongs]. setSongs is the method that is given back that I can use to update the local state.

--- src/components/SongList.jsx
+++ src/components/SongList.jsx
@@ -1,12 +1,24 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 export const SongList = () => {
+  const [songs, setSongs] = useState([
+    { title: 'Almost Home', id: 1 },
+    { title: 'Memory Gospel', id: 2 },
+    { title: 'This Wild Darkness', id: 3 }
+  ])
+  const addSong = () => {
+    setSongs([...songs, {title: 'new song', id: 4}], )
+  }
   return(
     <div className="song-list">
       <ul>
-        <li>This While Darkness</li>
-        <li>Memory Gospel</li>
+        {songs.map( song => {
+          return (
+            <li key={ song.id }>{ song.title }</li>
+          );
+        })}
       </ul>
+      <button onClick={ addSong }>Add a song</button>
     </div>
 
   )
